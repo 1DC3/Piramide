4df3955ac4c2ccf8fa71f457cb0ae79c16ff5c87
xDCx <13d13c13ll13@gmail.com>
2016-03-05 11:31:43 +0100

    Añadido nuevos métodos para la clase Piramide: Se han incorporado todas las combinaciones principales posibles.

--- src/piramide/Piramide.java
+++ src/piramide/Piramide.java
@@ -34,10 +34,61 @@ public class Piramide {
         System.out.println();
     }
     
+    public static void IzqI(int h){
+        for(int i=0; i<h; i++){
+            System.out.println();
+            for(int j=0; j<h-i; j++){
+                System.out.printf("%c", '*');
+            }
+        }
+        System.out.println();
+    }
+    
+    public static void DchI(int h){
+        for(int i=0; i<h; i++){
+            System.out.println();
+            for(int j=0; j<i; j++){
+                System.out.printf("%c", ' ');
+            }
+            for(int k=0; k<h-i; k++){
+                System.out.printf("%c", '*');
+            }
+        }
+        System.out.println();
+    }
+    
+    public static void Cnt(int h){
+        for(int i=0; i<h; i++){
+            System.out.println();
+            for(int j=h-1-i; j>0; j--){
+                System.out.printf("%c", ' ');
+            }
+            for(int j=2*i+1; j>0; j--)
+                System.out.print("*");
+        }
+        System.out.println();
+    }
+    public static void CntI(int h){
+        for(int i=0; i<h; i++){
+            System.out.println();
+            for(int j=0; j<i; j++){
+                System.out.printf("%c", ' ');
+            }
+            for(int j=0; j<(2*(h-i)-1); j++){
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
         final int ALTURA = 5;
         Piramide.Izq(ALTURA);
         Piramide.Dch(ALTURA);
+        Piramide.IzqI(ALTURA);
+        Piramide.DchI(ALTURA);
+        Piramide.Cnt(ALTURA);
+        Piramide.CntI(ALTURA);
     }
     
 }
